7d56489fa344cbd6b491f9b8cc2badb4133d5705
nnvsek <nnvsek@gmail.com>
2020-02-29 00:34:40 -0500

    connected to firebase and working sign in

--- app/src/main/java/com/example/hospitalmanagement/MainActivity.java
+++ app/src/main/java/com/example/hospitalmanagement/MainActivity.java
@@ -1,35 +1,104 @@
 package com.example.hospitalmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-EditText usernamex,passwordx;
+EditText Emailx,passwordx;
 Button signinx,signupx;
+Intent i;
+FirebaseAuth Fauth;
+ProgressBar pb1x;
+FirebaseAuth.AuthStateListener FauthStateListner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        usernamex = findViewById(R.id.username);
+        Emailx = findViewById(R.id.email);
         passwordx = findViewById(R.id.password);
         signinx = findViewById(R.id.signin);
         signupx = findViewById(R.id.signup);
+        pb1x = findViewById(R.id.pb1);
+        Fauth = FirebaseAuth.getInstance();
 
+        FauthStateListner = new FirebaseAuth.AuthStateListener() {
+
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = Fauth.getCurrentUser();
+                if(firebaseUser != null){
+                    Toast.makeText(MainActivity.this, "logged in ", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this, signup.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "details please ", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        };
         signinx.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                    String email = Emailx.getText().toString().trim();
+                    String password = passwordx.getText().toString().trim();
+
+                    if(TextUtils.isEmpty(email)){
+                        Emailx.setError("Need the email dude");
+                        return;
+
+                    }
+                    if(TextUtils.isEmpty(password)){
+                        passwordx.setError("Need the password dude");
+                        return;
+                    }
+
+                    pb1x.setVisibility(View.VISIBLE);
+
+                    Fauth.signInWithEmailAndPassword(email,password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                       if(!task.isSuccessful()){
+                           Toast.makeText(MainActivity.this, "not  success ", Toast.LENGTH_SHORT).show();
+                       }
+                       else{
+                           Intent i = new Intent(MainActivity.this, signup.class);
+                           startActivity(i);
+                       }
+                        }
+                    });
 
             }
         });
         signupx.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+Intent i = new Intent(MainActivity.this, signup.class);
+startActivity(i);
             }
         });
     }
+
+  /*  @Override
+    protected void onStart() {
+        super.onStart();
+        Fauth.addAuthStateListener(FauthStateListner );
+    }*/
 }
--- app/src/main/java/com/example/hospitalmanagement/signup.java
+++ app/src/main/java/com/example/hospitalmanagement/signup.java
@@ -1,33 +1,92 @@
 package com.example.hospitalmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signup extends AppCompatActivity {
-TextView t1,t2;
+TextView t1;
 EditText usernamex,passwordx,emailx,phonex;
-Button registerx;
+Button registerx,back;
+FirebaseAuth FAuth;
+Intent i,i1;
+ProgressBar pb2x;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        back = findViewById(R.id.backtosignin);
         usernamex = findViewById(R.id.username);
         passwordx = findViewById(R.id.password);
         emailx =  findViewById(R.id.email);
         phonex = findViewById(R.id.phone);
         registerx =        findViewById(R.id.register);
+        pb2x = findViewById(R.id.pb2);
+        FAuth = FirebaseAuth.getInstance();
+
+        if(FAuth.getCurrentUser() !=null){
+            Intent i1 = new Intent(signup.this,home.class);
+            startActivity(i1);
+        }
 
         registerx.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = emailx.getText().toString().trim();
+                String password = passwordx.getText().toString().trim();
 
+                if(TextUtils.isEmpty(email)){
+                    emailx.setError("Need the email dude");
+                    return;
+
+                }
+                if(TextUtils.isEmpty(password)){
+                    passwordx.setError("Need the password dude");
+                    return;
+                }
+
+            pb2x.setVisibility(View.VISIBLE);
+
+                FAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                         if(task.isSuccessful()){
+                             Toast.makeText(signup.this, "done", Toast.LENGTH_SHORT).show();
+                         Intent i1 = new Intent(signup.this,home.class);
+                         startActivity(i1);
+
+                         }
+                         else {
+                             Toast.makeText(signup.this,"ayyayyo",Toast.LENGTH_SHORT ).show();
+                         }
+                    }
+                });
             }
+
         });
+     back.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             Intent i = new Intent(signup.this, MainActivity.class);
+             startActivity(i);
+         }
+     });
     }
 }
